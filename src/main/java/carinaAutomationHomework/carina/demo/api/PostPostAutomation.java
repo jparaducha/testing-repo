c56a1e3e69b5be5bc33ecea66f5a8ca52f83c98a
jparaducha <paraducha_97@hotmail.com>
2022-10-27 12:54:12 -0300

    added annotations to post methods

--- src/main/java/carinaAutomationHomework/carina/demo/api/PostPostAutomation.java
+++ src/main/java/carinaAutomationHomework/carina/demo/api/PostPostAutomation.java
@@ -1,13 +1,20 @@
 package carinaAutomationHomework.carina.demo.api;
 
 import com.qaprosoft.carina.core.foundation.api.AbstractApiMethodV2;
+import com.qaprosoft.carina.core.foundation.api.annotation.RequestTemplatePath;
+import com.qaprosoft.carina.core.foundation.api.annotation.ResponseTemplatePath;
 import com.qaprosoft.carina.core.foundation.utils.Configuration;
 
+@RequestTemplatePath(path = "api/posts/_post/request.json")
+@ResponseTemplatePath(path = "api/posts/_post/response.json")
 public class PostPostAutomation extends AbstractApiMethodV2 {
     public PostPostAutomation(){
+        /*
         super("api/posts/_post/request.json",
                 "api/posts/_post/response.json",
                 "api/posts/user.properties");
+
+         */
         replaceUrlPlaceholder("base_url", Configuration.getEnvArg("api_url"));
     }
 }
--- src/main/java/carinaAutomationHomework/carina/demo/api/PostPostAutomation2.java
+++ src/main/java/carinaAutomationHomework/carina/demo/api/PostPostAutomation2.java
@@ -1,15 +1,21 @@
 package carinaAutomationHomework.carina.demo.api;
 
 import com.qaprosoft.carina.core.foundation.api.AbstractApiMethodV2;
+import com.qaprosoft.carina.core.foundation.api.annotation.RequestTemplatePath;
+import com.qaprosoft.carina.core.foundation.api.annotation.ResponseTemplatePath;
 import com.qaprosoft.carina.core.foundation.utils.Configuration;
 
 import java.util.Properties;
 
+@RequestTemplatePath(path = "api/posts/_post/request2.json")
+@ResponseTemplatePath(path = "api/posts/_post/response2.json")
 public class PostPostAutomation2 extends AbstractApiMethodV2 {
     public PostPostAutomation2 () {
+        /*
         super("api/posts/_post/request2.json",
                 "api/posts/_post/response2.json",
                 new Properties());
+         */
         replaceUrlPlaceholder("base_url", Configuration.getEnvArg("api_url"));
     }
 }
